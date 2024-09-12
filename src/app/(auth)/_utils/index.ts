c58decd5b7745ba7d261b4db041c301355d31f6b
aptimizer <aptimizer.co@gmail.com>
2024-09-12 15:34:22 +0900

    [✨기능] URL에서 accessToken을 가져오는 함수

--- src/app/(auth)/_utils/index.ts
+++ src/app/(auth)/_utils/index.ts
@@ -1,2 +1,3 @@
+export { default as getAccessTokenFromURL } from "./getAccessTokenFromURL/getAccessTokenFromURL";
 export { default as getAccessTokenFromLocalStorage } from "./getAccessTokenFromLocalStorage/getAccessTokenFromLocalStorage";
 export { default as saveAccessTokenToLocalStorage } from "./saveAccessTokenToLocalStorage/saveAccessTokenToLocalStorage";
