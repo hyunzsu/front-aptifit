7fea726d453482c78ce66cd0b1a464fb61d9aa6a
aptimizer <aptimizer.co@gmail.com>
2024-09-12 14:37:51 +0900

    [✨기능] OAuth 리다이렉트 URL 변경
    OAuth API가 여러 개인 걸 고려해 /oauth에서 /oauth/[provider]로 처리할 수 있도록 리다이렉트 URL을 수정했다.

--- src/app/(auth)/login/_components/KakaoLoginButton/KakaoLoginButton.tsx
+++ src/app/(auth)/login/_components/KakaoLoginButton/KakaoLoginButton.tsx
@@ -3,7 +3,7 @@
 export default function KakaoLoginButton() {
   const KAKAO_CLIENT_ID = `${process.env.NEXT_PUBLIC_API_KAKAO_CLIENT_ID}`;
   const REDIRECT_URI = `${process.env.NEXT_PUBLIC_API_REDIRECT_URI}`;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}/kakao&response_type=code`;
 
   const handleKakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
--- src/app/(auth)/login/_components/NaverLoginButton/NaverLoginButton.tsx
+++ src/app/(auth)/login/_components/NaverLoginButton/NaverLoginButton.tsx
@@ -4,7 +4,7 @@ export default function NaverLoginButton() {
   const NAVER_CLIENT_ID = `${process.env.NEXT_PUBLIC_API_NAVER_CLIENT_ID}`; // 발급받은 클라이언트 아이디
   const REDIRECT_URI = `${process.env.NEXT_PUBLIC_API_REDIRECT_URI}`; // Callback URL
   const STATE = "false";
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}/naver`;
 
   const handleNaverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
