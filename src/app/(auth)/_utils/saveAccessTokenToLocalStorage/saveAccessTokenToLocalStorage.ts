0d465dcec915cc032a9d9fbd581500825a94c139
aptimizer <aptimizer.co@gmail.com>
2024-09-12 17:47:17 +0900

    [✨기능] OAuth 로그인을 성공할 경우 accessToken을 로컬스토리지에 저장하는 콜백기능
    서버에서 OAuth 로그인을 성공했다면 사용자는 URL에  /oauth/complete로 리다이렉트가 된다.
    
    /OAuthComplete에서는 URL에 담긴 accessToken을 로컬스토리지에 저장한 다음 메인 페이지로 이동을 시킨다.

--- src/app/(auth)/_utils/saveAccessTokenToLocalStorage/saveAccessTokenToLocalStorage.ts
+++ src/app/(auth)/_utils/saveAccessTokenToLocalStorage/saveAccessTokenToLocalStorage.ts
@@ -1,10 +1,10 @@
 import { getAccessTokenFromURL } from "../index";
 
 const saveAccessTokenToLocalStorage = () => {
-  // accessToken URL로부터 추출
+  // 1. accessToken URL로부터 추출
   const accessToken = getAccessTokenFromURL();
 
-  // accessToken 로컬 스토리지에 저장
+  // 2. accessToken 로컬 스토리지에 저장
   localStorage.setItem("accessToken", JSON.stringify(accessToken));
 };
 
--- src/app/(auth)/oauth/complete/page.tsx
+++ src/app/(auth)/oauth/complete/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { saveAccessTokenToLocalStorage } from "../../_utils";
+
 export default function OAuthComplete() {
-  return <div>OAuthComplete</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    // 1. URL담긴 accessToken을 로컬스토리지에 저장
+    saveAccessTokenToLocalStorage();
+
+    // 2. accessToken을 로컬스토리지에 저장한 후 메인페이지로 이동
+    router.push("/");
+  }, []);
+
+  return <main>OAuthComplete</main>;
 }
