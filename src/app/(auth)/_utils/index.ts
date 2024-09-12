91d6ae7fd6ec7a472312dd4786b11b1766ce9fd3
aptimizer <aptimizer.co@gmail.com>
2024-09-12 15:34:40 +0900

    [✨기능] URL에서 OAuth 인증코드를 가져오는 함수

--- src/app/(auth)/_utils/index.ts
+++ src/app/(auth)/_utils/index.ts
@@ -1,3 +1,4 @@
 export { default as getAccessTokenFromURL } from "./getAccessTokenFromURL/getAccessTokenFromURL";
+export { default as getOAuthCodeFromURL } from "./getOAuthCodeFromURL/getOAuthCodeFromURL";
 export { default as getAccessTokenFromLocalStorage } from "./getAccessTokenFromLocalStorage/getAccessTokenFromLocalStorage";
 export { default as saveAccessTokenToLocalStorage } from "./saveAccessTokenToLocalStorage/saveAccessTokenToLocalStorage";
