470ddda31a098c04d00e3457fa24f6eecc60135b
aptimizer <aptimizer.co@gmail.com>
2024-09-13 20:49:09 +0900

    [🎨UI] 홈페이지에서 사용되는 애니메이션 UI 완성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import { MajorCardSlider } from "./_components";
 import s from "./HomePage.module.css";
 
 export default function HomePage() {
-  return <main className={s.HomePage}>메인 페이지</main>;
+  return (
+    <main className={s.HomePage}>
+      메인 페이지
+      <Image
+        className={s.iphone}
+        src="/imgs/iphone.png"
+        alt="아이폰"
+        width={320}
+        height={727}
+      />
+      <div className={s.cardAnimationContainer}>
+        <MajorCardSlider />
+      </div>
+    </main>
+  );
 }
