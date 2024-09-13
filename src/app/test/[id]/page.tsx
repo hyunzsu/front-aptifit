fe69ab5b107c83b6cfe14b95d54b9b58c23dfd5e
kimzeze <fridaynight@kakao.com>
2024-09-13 14:42:20 +0900

    [🎨UI] 테스트 페이지에 컴포넌트 테스트를 위한 임시 마크업

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,5 +1,24 @@
-import s from "./TestDetailPage.module.css";
+import s from "./test.module.css";
+import TestTitle from "./_components/TestTitle/TestTitle";
+import QuizItem from "./_components/QuizItem/QuizItem";
+import ProgressBar from "./_components/ProgressBar/ProgressBar";
+import Button from "@/components/Button/Button";
 
-export default function TestDetailPage() {
-  return <main className={s.TestDetailPage}>테스트 상세 페이지</main>;
+export default function Test() {
+  return (
+    <div className={s.test}>
+      <section className={s.testSection}>
+        <TestTitle />
+        <ProgressBar />
+        <QuizItem />
+        <QuizItem />
+        <QuizItem />
+        <QuizItem />
+        <QuizItem />
+        <QuizItem />
+      </section>
+      {/* 테스트 페이지 버튼 */}
+      <Button label="다음" type="button" pageType="test" />
+    </div>
+  );
 }
