2a768a436a349343c55887f12acd70b83a51b87c
aptimizer <aptimizer.co@gmail.com>
2024-09-14 17:07:08 +0900

    [🎨UI] 홈페이지 UI 완성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,11 @@ import s from "./HomePage.module.css";
 export default function HomePage() {
   return (
     <main className={s.HomePage}>
-      메인 페이지
+      <div className={s.titleContainer}>
+        <p className={s.subtitle}>AI 기반 진단도구로 쉽고 빠르게</p>
+        <p className={s.title}>너만의 전공적성을 찾아봐!</p>
+        <button className={s.button}>시작하기</button>
+      </div>
       <Image
         className={s.iphone}
         src="/imgs/iphone.png"
