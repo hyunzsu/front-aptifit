cf8306a3b39ba56a709f142b367299ea93ad50b5
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-16 11:45:28 +0900

    [✨기능] 드롭다운필터 컴포넌트 생성

---
+++ src/app/add-user-info/_components/DropdownFilter/DropdownFilter.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import s from "./DropdownFilter.module.css";
+
+type TDropdownFilter = {
+  defaultValue: string;
+  data: string[];
+};
+
+export default function DropdownFilter({
+  defaultValue,
+  data,
+}: TDropdownFilter) {
+  const [selectedValue, setIsSelectedValue] = useState(defaultValue);
+  const [isFilterActive, setIsFilterActive] = useState(false);
+
+  const handleFilterActive = () => {
+    setIsFilterActive(!isFilterActive);
+  };
+
+  const handleSelectedValue = (e) => {
+    setIsSelectedValue(e.target.textContent);
+    setIsFilterActive(false);
+  };
+
+  return (
+    <div
+      className={`${s.DropdownFilter} ${
+        isFilterActive ? s.DropdownFilterActive : ""
+      }`}
+    >
+      <div className={s.topContainer}>
+        <span>{selectedValue}</span>
+        {isFilterActive ? (
+          <button className={s.button} onClick={handleFilterActive}>
+            <Image src="/icons/arrow-down.svg" alt="" width={24} height={24} />
+          </button>
+        ) : (
+          <button className={s.button} onClick={handleFilterActive}>
+            <Image src="/icons/arrow-up.svg" alt="" width={24} height={24} />
+          </button>
+        )}
+      </div>
+
+      {isFilterActive && (
+        <ul className={s.bottomContainer}>
+          {data.map((value, index) => {
+            return (
+              <li
+                key={`${index}-${value}`}
+                className={s.item}
+                onClick={(e) => handleSelectedValue(e)}
+              >
+                {value}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
