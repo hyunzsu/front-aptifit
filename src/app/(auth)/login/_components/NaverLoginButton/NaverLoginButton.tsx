b6432dcb668d27d47ff80722191674387c6bb112
aptimizer <aptimizer.co@gmail.com>
2024-09-20 14:38:08 +0900

    [🎨UI] 네이버로그인버튼 UI

--- src/app/(auth)/login/_components/NaverLoginButton/NaverLoginButton.tsx
+++ src/app/(auth)/login/_components/NaverLoginButton/NaverLoginButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import s from "./NaverLoginButton.module.css";
+
 export default function NaverLoginButton() {
   const NAVER_CLIENT_ID = `${process.env.NEXT_PUBLIC_API_NAVER_CLIENT_ID}`; // 발급받은 클라이언트 아이디
   const REDIRECT_URI = `${process.env.NEXT_PUBLIC_API_REDIRECT_URI}`; // Callback URL
@@ -10,5 +12,9 @@ export default function NaverLoginButton() {
     window.location.href = NAVER_AUTH_URL;
   };
 
-  return <button onClick={handleNaverLogin}>네이버 로그인</button>;
+  return (
+    <button className={s.NaverLoginButton} onClick={handleNaverLogin}>
+      N
+    </button>
+  );
 }
