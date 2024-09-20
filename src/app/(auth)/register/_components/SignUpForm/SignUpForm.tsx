cccf46906daeaa582e4682f13b794a1d0777c811
hyunzsu <hyunzsu@naver.com>
2024-09-20 15:58:31 +0900

    [✨기능] react-hook-form 실시간 유효성 검사 모드 추가

--- src/app/(auth)/register/_components/SignUpForm/SignUpForm.tsx
+++ src/app/(auth)/register/_components/SignUpForm/SignUpForm.tsx
@@ -8,11 +8,12 @@ import s from "./SignUpForm.module.css";
 
 export default function SignUpForm() {
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
+    register, // 입력 필드 등록 함수
+    handleSubmit, // 폼 제출
+    formState: { errors, isSubmitting }, // 폼 상태 (에러, 제출중 여부)
   } = useForm<SignupFormData>({
-    resolver: zodResolver(signupSchema),
+    resolver: zodResolver(signupSchema), // zod 스키마 사용한 유효성 검사 설정
+    mode: "onChange", // 실시간 유효성 검사
   });
 
   const onSubmit = (data: SignupFormData) => {
@@ -27,6 +28,14 @@ export default function SignUpForm() {
         register={register}
         error={errors.username}
         placeholder="사용자 이름을 입력하세요"
+      />
+      <FormInput
+        label="휴대폰 번호"
+        name="phoneNumber"
+        type="tel"
+        register={register}
+        error={errors.phoneNumber}
+        placeholder="휴대폰 번호를 입력하세요"
       />
       <FormInput
         label="이메일"
@@ -52,8 +61,26 @@ export default function SignUpForm() {
         error={errors.confirmPassword}
         placeholder="비밀번호를 다시 입력하세요"
       />
-      <button type="submit" className={s.button}>
-        가입하기
+      <div className={s.agreementSection}>
+        <h3 className={s.h3}>약관 동의</h3>
+        <FormInput
+          label="앱티핏 이용약관에 동의 (필수)"
+          name="agreeToTerms"
+          register={register}
+          error={errors.agreeToTerms}
+          isCheckbox={true}
+        />
+        <FormInput
+          label="개인정보 수집 및 이용에 동의 (필수)"
+          name="agreeToPrivacyPolicy"
+          register={register}
+          error={errors.agreeToPrivacyPolicy}
+          isCheckbox={true}
+        />
+      </div>
+
+      <button disabled={isSubmitting} type="submit" className={s.button}>
+        {isSubmitting ? "Loading..." : "가입하기"}
       </button>
     </form>
   );
