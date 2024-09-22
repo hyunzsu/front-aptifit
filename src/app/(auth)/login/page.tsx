3936e8045860f620e623348a511e30e3549abf64
hyunzsu <hyunzsu@naver.com>
2024-09-23 04:40:19 +0900

    [🎨UI] 로그인 페이지 생성

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,14 @@
+import { Title } from "@/components";
 import s from "./LoginPage.module.css";
+import LoginForm from "./_components/LoginForm/LoginForm";
 
 export default function LoginPage() {
-  return <main className={s.LoginPage}>로그인 페이지</main>;
+  return (
+    <main className={s.loginPage}>
+      <div className={s.container}>
+        <Title label="로그인" />
+        <LoginForm />
+      </div>
+    </main>
+  );
 }
