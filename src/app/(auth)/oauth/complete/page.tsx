3d0d113fabc4b30ea90d8056f235899bc26a059b
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-24 00:20:45 +0900

    [🛠리팩토링] /oauth/complete 페이지 리팩토링

--- src/app/(auth)/oauth/complete/page.tsx
+++ src/app/(auth)/oauth/complete/page.tsx
@@ -2,23 +2,21 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import {
-  saveAccessTokenToLocalStorage,
-  getAccessTokenFromLocalStorage,
-} from "../../_utils";
+import { saveToSessionStorage } from "@/lib/utils";
 
 export default function OAuthComplete() {
   const router = useRouter();
 
+  const accessToken = new URL(window.location.href).searchParams.get(
+    "access_token"
+  );
+
   useEffect(() => {
     // 1. URL담긴 accessToken을 로컬스토리지에 저장
-    saveAccessTokenToLocalStorage();
+    saveToSessionStorage("access_token", JSON.stringify(accessToken));
 
     // 2. accessToken을 로컬스토리지에 저장한 후 메인페이지로 이동
-    if (!!getAccessTokenFromLocalStorage()) {
-      console.log(!!getAccessTokenFromLocalStorage());
-      router.push("/");
-    }
+    router.push("/");
   }, []);
 
   return <main>OAuthComplete</main>;
