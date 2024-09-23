4a8fd16fd724a8bd59ccf960d62c47e0ffcbdcf0
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-24 00:49:29 +0900

    [✨기능] 홈페이지(/)에서 테스트 페이지(/test)로 이동 기능 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components";
 import { MajorCardSlider } from "./_components";
 import s from "./HomePage.module.css";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const startTest = () => {
+    router.push("/test");
+  };
+
   return (
     <main className={s.HomePage}>
       <div className={s.titleContainer}>
         <p className={s.subtitle}>AI 기반 진단도구로 쉽고 빠르게</p>
         <p className={s.title}>너만의 전공적성을 찾아봐</p>
-        <button className={s.button}>검사 시작하기</button>
+        <Button
+          label="검사 시작하기"
+          type="button"
+          pageType="main"
+          onClick={startTest}
+        />
       </div>
       <Image
         className={s.iphone}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Button } from "./Button/Button";
 export { default as LayoutContainer } from "./LayoutContainer/LayoutContainer";
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as Title } from "./Title/Title";
