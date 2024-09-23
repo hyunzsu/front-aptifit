f1bd898cf38ae653f1d4b79fdedc77574ecb7e93
aptimizer <aptimizer.co@gmail.com>
2024-09-23 17:55:10 +0900

    [✨기능] 제한된 라우트 기능을 구현하는 useRedirectIfNotAuthenticated 커스텀훅 생성
    인증되지 않은 사용자가 들어올 경우, /login으로 리다이렉트 시키는 커스텀훅을 생성했다.

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,2 @@
+export { default as useAccessToken } from "./useAccessToken/useAccessToken";
+export { default as useRedirectIfNotAuthenticated } from "./useRedirectIfNotAuthenticated/useRedirectIfNotAuthenticated";
