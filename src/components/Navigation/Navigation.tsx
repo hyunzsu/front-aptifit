b2eaddc04776b092febd39939cebc3327c8b59cf
aptimizer <aptimizer.co@gmail.com>
2024-09-23 12:16:51 +0900

    [✨기능] 네비게이션 기능 추가
    Link 컴포넌트를 사용해 네비게이션 기능을 구현했다.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import s from "./Navigation.module.css";
 
 export default function Navigation() {
   return (
     <nav className={s.Navigation}>
       <div className={s.innerContainer}>
-        <h1 className={s.logo}>APTIFIT</h1>
+        <h1 className={s.logo}>
+          <Link href="/">APTIFIT</Link>
+        </h1>
         <ul className={s.ul}>
-          <li className={s.li}>결과지</li>
-          <li className={s.li}>로그인</li>
+          <li className={s.li}>
+            <Link className={s.link} href="/result">
+              결과지
+            </Link>
+          </li>
+          <li className={s.li}>
+            <Link className={s.link} href="/login">
+              로그인
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
