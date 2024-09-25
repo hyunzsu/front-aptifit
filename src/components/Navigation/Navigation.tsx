c81df4abfe4655c2e96315bbb0f647afd5ecfae7
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-26 01:19:04 +0900

    [✨기능] 네비게이션에 로그아웃 기능 추가
    사용자의 로그인 유무에 따라 조건부 렌더링을 수행해 로그인 링크 혹은 로그아웃 버튼이 렌더링된다.
    
    또한, 로그아웃을 클릭할 경우 handleLogout 함수가 동작해 세션스토리지에 보관된 데이터를 제거하고, 서버와 통신해 refresh_token을 밴한다.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,42 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/stores";
 import s from "./Navigation.module.css";
 
 export default function Navigation() {
+  const { isLoggedIn, logout } = useAuthStore();
+  const router = useRouter();
+
+  /* 로그아웃 함수 */
+  const handleLogout = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/logout`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const result = await response.json();
+
+      if (response.ok) {
+        logout();
+        alert("로그아웃이 됐습니다!");
+        router.push("/");
+      } else {
+        console.error("에러 발생:", result.error);
+        alert(result.error);
+      }
+    } catch (error) {
+      console.error("데이터 전송 중 오류가 발생했습니다:", error);
+    }
+  };
+
   return (
     <nav className={s.Navigation}>
       <div className={s.innerContainer}>
@@ -14,11 +49,19 @@ export default function Navigation() {
               결과지
             </Link>
           </li>
-          <li className={s.li}>
-            <Link className={s.link} href="/login">
-              로그인
-            </Link>
-          </li>
+          {!isLoggedIn ? (
+            <li className={s.li}>
+              <Link className={s.link} href="/login">
+                로그인
+              </Link>
+            </li>
+          ) : (
+            <li className={s.li}>
+              <button className={s.link} onClick={handleLogout}>
+                로그아웃
+              </button>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
