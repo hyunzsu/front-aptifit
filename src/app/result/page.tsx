309dc869086a1d15f5a67e11d4fe6f8a207c81ab
kimzeze <fridaynight@kakao.com>
2024-09-25 13:29:32 +0900

    [🎨UI] 결과지 섹션 파일 생성

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,5 +1,13 @@
 import s from "./ResultPage.module.css";
+import { Section01, Section02, Section03, Section04 } from "./sections";
 
 export default function ResultPage() {
-  return <main className={s.ResultPage}>결과 페이지</main>;
+  return (
+    <main className={s.ResultPage}>
+      <Section01 />
+      <Section02 />
+      <Section03 />
+      <Section04 />
+    </main>
+  );
 }
---
+++ src/app/result/sections/Section04.tsx
@@ -0,0 +1,8 @@
+export default function Section04() {
+  return (
+    <div>
+      <h2>Section 04</h2>
+      <p>This is Section 04</p>
+    </div>
+  );
+}
