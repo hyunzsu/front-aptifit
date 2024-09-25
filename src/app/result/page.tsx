3ac619546ac52b54cb9caa7abdbb126879f80e6a
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-26 01:20:48 +0900

    [🔫트러블슈팅] 라우트 보호기능 개선
    로그인하지 않은 사용자가 라우트에 접근하는 걸 방지하는 기능이다.
    
    isLoggedIn 전역상태변수의 초깃값은 세션스토리지에 저장된 access_token으로 새로고침이 발생해도 상태변수의 값이 사라지지 않아 리디렉션이 되던 문제를 해결했다.

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import { useAuthStore } from "@/lib/stores";
+import { useRouter } from "next/navigation";
 import s from "./ResultPage.module.css";
 
 export default function ResultPage() {
+  const { isLoggedIn } = useAuthStore();
+  const router = useRouter();
+
+  if (!isLoggedIn) {
+    router.push("/");
+    return null;
+  }
+
   return <main className={s.ResultPage}>결과 페이지</main>;
 }
