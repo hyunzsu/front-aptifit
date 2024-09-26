b06cdaffbd010ff0f5d91c488abea800c2a7df05
aptimizer <aptimizer.co@gmail.com>
2024-09-26 11:55:28 +0900

    [🛡️타입] useAuthStore 유저 데이터 타입 지정

--- src/lib/stores/useAuthStore/useAuthStore.ts
+++ src/lib/stores/useAuthStore/useAuthStore.ts
@@ -1,15 +1,20 @@
 import { create } from "zustand";
 
+type TUser = {
+  email: string;
+  name: string;
+};
+
 type TAuthState = {
   isLoggedIn: boolean;
-  login: (token: string, user: string) => void;
+  login: (token: string, user: TUser) => void;
   logout: () => void;
 };
 
 // Zustand 스토어 생성
 const useAuthStore = create<TAuthState>((set) => ({
   isLoggedIn: !!sessionStorage.getItem("access_token"), // 세션스토리지를 바탕으로 초깃값을 두어 세션 스토리지에 값이 있다면 새로고침해도 false X
-  login: (token: string, user: string) => {
+  login: (token, user) => {
     sessionStorage.setItem("access_token", token);
     sessionStorage.setItem("user", JSON.stringify(user));
     set({ isLoggedIn: true });
