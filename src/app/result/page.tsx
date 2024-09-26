39dc1146d1eb2fda8eaaf34409b9cdfab2b6f2cc
aptimizer <aptimizer.co@gmail.com>
2024-09-26 21:03:18 +0900

    [🎸기타] 세미콜론 제거

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -4,7 +4,7 @@ import s from "./ResultPage.module.css";
 export default function ResultPage() {
   return (
     <ProtectedRoute>
-      <main className={s.ResultPage}>결과 페이지</main>;
+      <main className={s.ResultPage}>결과 페이지</main>
     </ProtectedRoute>
   );
 }
