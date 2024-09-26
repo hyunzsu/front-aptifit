b892d75a3c82e9180c79aeb51be5ae9c253828c7
aptimizer <aptimizer.co@gmail.com>
2024-09-26 10:13:33 +0900

    [🛡️타입] useAuthStore
    useAuthStore을 사용하면 unknown 타입 에러를 명시적인 타입 지정으로 해결했다.

--- src/lib/stores/useAuthStore/useAuthStore.ts
+++ src/lib/stores/useAuthStore/useAuthStore.ts
@@ -1,7 +1,13 @@
 import { create } from "zustand";
 
+type TAuthState = {
+  isLoggedIn: boolean;
+  login: (token: string, user: string) => void;
+  logout: () => void;
+};
+
 // Zustand 스토어 생성
-const useAuthStore = create((set) => ({
+const useAuthStore = create<TAuthState>((set) => ({
   isLoggedIn: !!sessionStorage.getItem("access_token"), // 세션스토리지를 바탕으로 초깃값을 두어 세션 스토리지에 값이 있다면 새로고침해도 false X
   login: (token: string, user: string) => {
     sessionStorage.setItem("access_token", token);
