f1cb28caed9e563f7226852fc185ffe46f74e0be
aptimizer <aptimizer.co@gmail.com>
2024-09-26 18:47:13 +0900

    [✨기능] useAuthStore 상태변수값 수정

--- src/lib/stores/useAuthStore/useAuthStore.ts
+++ src/lib/stores/useAuthStore/useAuthStore.ts
@@ -1,28 +1,33 @@
 import { create } from "zustand";
 
 type TUser = {
-  email: string;
-  user_name: string;
+  name: string;
+  user_id: string;
+  IsPayment: boolean;
+  IsAdditionalUserInfo: boolean;
+  page: number;
+  responses?: number[];
+  questions?: string[];
+  result?: any;
+  access_token: string;
 };
 
 type TAuthState = {
   isLoggedIn: boolean;
-  login: (token: string, user: TUser) => void;
+  login: (user: TUser) => void;
   logout: () => void;
 };
 
 // Zustand 스토어 생성
 const useAuthStore = create<TAuthState>((set) => ({
-  isLoggedIn: !!sessionStorage.getItem("access_token"), // 세션스토리지를 바탕으로 초깃값을 두어 세션 스토리지에 값이 있다면 새로고침해도 false X
-  login: (token, user) => {
-    sessionStorage.setItem("access_token", token);
-    sessionStorage.setItem("user", JSON.stringify(user));
-    set({ isLoggedIn: true });
+  isLoggedIn: !!sessionStorage.getItem("user"), // 세션스토리지를 바탕으로 초깃값을 두어 세션 스토리지에 값이 있다면 새로고침해도 false X
+  login: (userData) => {
+    sessionStorage.setItem("user", JSON.stringify(userData));
+    set({ isLoggedIn: !!sessionStorage.getItem("user") });
   },
   logout: () => {
-    sessionStorage.removeItem("access_token");
     sessionStorage.removeItem("user");
-    set({ isLoggedIn: false });
+    set({ isLoggedIn: !!sessionStorage.getItem("user") });
   },
 }));
 
