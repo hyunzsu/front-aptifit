8e32325ccb9e31673d247a881f4a4a4269923050
aptimizer <aptimizer.co@gmail.com>
2024-09-26 10:06:42 +0900

    [✨기능] ProtectedRoute 컴포넌트 생성
    로그인하지 않은 사용자가 페이지에 접근하고 해당 페이지가 렌더링되는 걸 막기위해 ProtectedRoute 컴포넌트를 생성했다.
    
    해당 컴포넌트는 useAuthStore에 담긴 isLoggedIn 상태변수를 참조하여 동작한다.
    
    isLoggedIn이 false 상태라면 홈(/)으로 리디렉트를 하고, return null을 해 렌더린을 방지한다.

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/stores";
+
+const ProtectedRoute = ({ children }) => {
+  const { isLoggedIn } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/login"); // 로그인 페이지로 리디렉션
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    return null; // 리디렉션 중에는 아무것도 렌더링하지 않음
+  }
+
+  return <>{children}</>; // 로그인 상태이면 자식 컴포넌트를 렌더링
+};
+
+export default ProtectedRoute;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/Button";
 export { default as LayoutContainer } from "./LayoutContainer/LayoutContainer";
+export { default as ProtectedRoute } from "./ProtectedRoute/ProtectedRoute";
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as Title } from "./Title/Title";
