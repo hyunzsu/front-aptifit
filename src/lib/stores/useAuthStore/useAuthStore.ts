328e2da86c79538b27ee97f672c27df99390ba5c
aptimizer <aptimizer.co@gmail.com>
2024-09-26 10:05:07 +0900

    [🛠️리팩토링] useAuthStore 개선
    초깃값을 불린형으로 변경했다. 하지만 여전히 세션스토리를 참조하기 때문에 새로고침을 하더라도 false가 되는 문제는 없다.
    
    isLoggedIn 상태변수만으로도 사용자의 로그인 유무파악이 가능하기 때문에 checkUser 함수를 제거했다.

--- src/lib/stores/useAuthStore/useAuthStore.ts
+++ src/lib/stores/useAuthStore/useAuthStore.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 // Zustand 스토어 생성
 const useAuthStore = create((set) => ({
-  isLoggedIn: sessionStorage.getItem("access_token"), // 로그인 상태 기본값
+  isLoggedIn: !!sessionStorage.getItem("access_token"), // 세션스토리지를 바탕으로 초깃값을 두어 세션 스토리지에 값이 있다면 새로고침해도 false X
   login: (token: string, user: string) => {
     sessionStorage.setItem("access_token", token);
     sessionStorage.setItem("user", JSON.stringify(user));
@@ -11,11 +11,7 @@ const useAuthStore = create((set) => ({
   logout: () => {
     sessionStorage.removeItem("access_token");
     sessionStorage.removeItem("user");
-    set({ isLoggedIn: "" });
-  },
-  checkLoginStatus: () => {
-    const access_token = sessionStorage.getItem("access_token");
-    set({ isLoggedIn: !!access_token }); // 토큰이 있으면 true, 없으면 false
+    set({ isLoggedIn: false });
   },
 }));
 
