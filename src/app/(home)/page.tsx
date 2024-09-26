423852649e2f688a6a5783cfc9ef2f19fcfac776
aptimizer <aptimizer.co@gmail.com>
2024-09-26 18:47:46 +0900

    [✨기능] 테스트 시작 전에 예외처리 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,13 +4,57 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components";
 import { MajorCardSlider } from "./_components";
+import { useAuthStore } from "@/lib/stores";
 import s from "./HomePage.module.css";
 
 export default function HomePage() {
   const router = useRouter();
+  const { isLoggedIn } = useAuthStore();
 
   const startTest = () => {
-    router.push("/test");
+    const {
+      isPayment,
+      IsAdditionalUserInfo,
+      page,
+      responses,
+      questions,
+      result,
+    } = isLoggedIn;
+
+    // 1. 로그인 유무 파악 -> /login
+    if (!isLoggedIn) {
+      alert("로그인 페이지로 이동합니다!");
+      router.push("/login");
+      return;
+    }
+
+    // 2. 추가 정보 파악
+    if (!IsAdditionalUserInfo) {
+      alert("아직 입력하지 않은 정보가 있습니다!");
+      router.push("/add-user-info");
+      return;
+    }
+
+    //3. 결제 유무 파악 -> /payment
+    if (!isPayment) {
+      alert("결제정보가 없습니다!");
+      router.push("/payment");
+      return;
+    }
+
+    //4. 테스트 페이지 이동
+    if (page > 10 && responses && questions) {
+      alert("테스트 페이지로 이동합니다!");
+      router.push(`/test/${page}`);
+      return;
+    }
+
+    //5. 다 풀고, 결과지 데이터가 있으면 result로 이동
+    if (page === 10 && result) {
+      alert("결과지 페이지로 이동합니다!");
+      router.push(`/result`);
+      return;
+    }
   };
 
   return (
