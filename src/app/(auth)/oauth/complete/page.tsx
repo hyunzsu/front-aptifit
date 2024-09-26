5ab749e797ff2e6ccbd7628ecb8ed6efcf55e8a3
aptimizer <aptimizer.co@gmail.com>
2024-09-26 15:09:40 +0900

    [✨기능] OAuth 로그인 로직에 들어가는 데이터 명칭 변경

--- src/app/(auth)/oauth/complete/page.tsx
+++ src/app/(auth)/oauth/complete/page.tsx
@@ -11,10 +11,11 @@ export default function OAuthComplete() {
   useEffect(() => {
     const accessTokenURL =
       new URL(window.location.href).searchParams.get("access_token") || "";
-    const name = new URL(window.location.href).searchParams.get("name") || "";
+    const user_name =
+      new URL(window.location.href).searchParams.get("user_name") || "";
     const email = new URL(window.location.href).searchParams.get("email") || "";
 
-    login(accessTokenURL, { name: name, email: email });
+    login(accessTokenURL, { user_name: user_name, email: email });
 
     // router.push("/");
     alert("로그인 성공!");
