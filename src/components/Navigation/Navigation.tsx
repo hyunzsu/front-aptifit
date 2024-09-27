f05d727db473564246526e1f6ea207263ce7c24c
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-28 01:40:21 +0900

    [🎨UI] 네비게이션 UI 컬러 테마 적용

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useLogout } from "@/lib/hooks";
 import { useAuthStore } from "@/lib/stores";
@@ -8,6 +9,13 @@ import s from "./Navigation.module.css";
 export default function Navigation() {
   const { user } = useAuthStore();
   const { handleLogout } = useLogout();
+  const pathname = usePathname();
+
+  const logoTheme =
+    pathname === "/" || pathname === "/result" ? s.whiteLogo : s.blackLogo;
+
+  const textTheme =
+    pathname === "/" || pathname === "/result" ? s.whiteText : s.blackText;
 
   /* 로그아웃 함수 */
   const logout = () => {
@@ -17,29 +25,27 @@ export default function Navigation() {
   return (
     <nav className={`${s.Navigation}`}>
       <div className={s.innerContainer}>
-        <h1 className={s.logo}>
+        <h1 className={`${s.logo} ${logoTheme}`}>
           <Link href="/">APTIFIT</Link>
         </h1>
         <ul className={s.ul}>
           <li className={`${s.li}`}>
-            <Link className={s.link} href="/coupon">
-              쿠폰 등록
-            </Link>
-          </li>
-          <li className={`${s.li}`}>
-            <Link className={s.link} href="/result">
+            <Link className={`${s.link} ${textTheme}`} href="/result">
               결과지
             </Link>
           </li>
           {!user ? (
             <li className={s.li}>
-              <Link className={s.link} href="/login">
+              <Link className={`${s.link} ${textTheme}`} href="/login">
                 로그인
               </Link>
             </li>
           ) : (
             <li className={s.li}>
-              <button className={s.logoutButton} onClick={logout}>
+              <button
+                className={`${s.logoutButton} ${textTheme}`}
+                onClick={logout}
+              >
                 로그아웃
               </button>
             </li>
