f84fc403cd70b111853669942a662f7527e5542c
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-28 00:54:03 +0900

    [✨기능] 로그아웃 기능을 수행하는 useLogout 커스텀훅 생성

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,5 +1,6 @@
 export { default as useCoupon } from "./useCoupon/useCoupon";
 export { default as useLogin } from "./useLogin/useLogin";
+export { default as useLogout } from "./useLogout/useLogout";
 export { default as useRegister } from "./useRegister/useRegister";
 export { default as useStartTest } from "./useStartTest/useStartTest";
 export { default as useQuizLogic } from "@/lib/hooks/useQuizLogic";
