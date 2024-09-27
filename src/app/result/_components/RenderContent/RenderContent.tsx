cb51e8bb3c280701ab5f1a554dee4f81943b212f
hyunzsu <hyunzsu@naver.com>
2024-09-27 13:45:14 +0900

    [✨기능] RenderContent 컴포넌트 생성 및 기능 구현

---
+++ src/app/result/_components/RenderContent/RenderContent.tsx
@@ -0,0 +1,50 @@
+import { CategoryData, DetailItem } from "@/lib/types";
+import s from "./RenderContent.module.css";
+import Card from "@/components/Card/Card";
+import { Category } from "@/lib/constants/categories";
+
+type RenderContentProps = {
+  categoryData: CategoryData;
+  activeCategory: Category;
+};
+
+export default function RenderContent({
+  categoryData,
+  activeCategory,
+}: RenderContentProps) {
+  return (
+    <div className={s.categoryContent}>
+      {/* 분포도 - 차트 */}
+
+      {/* 분포도 - 카드 */}
+      <div className={s.cardContainer}>
+        <Card
+          title={`A. 나의 ${activeCategory} 분포도`}
+          description={
+            <div className={s.detailGrid}>
+              {categoryData.details.map((detail: DetailItem) => (
+                <div
+                  key={`${detail.field}-${detail.score}`}
+                  className={s.detail}
+                >
+                  <div className={s.fieldContainer}>
+                    <h3 className={s.field}>{detail.field}</h3>
+                    <span className={s.score}>{detail.score}점</span>
+                  </div>
+                  <p className={s.content}>{detail.content}</p>
+                </div>
+              ))}
+            </div>
+          }
+        />
+      </div>
+      {/* 심층분석 */}
+      <div className={s.description}>
+        <Card
+          title={`B. 나의 ${activeCategory} 심층분석`}
+          description={categoryData.descriptions}
+        />
+      </div>
+    </div>
+  );
+}
