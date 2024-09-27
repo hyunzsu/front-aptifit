09337d1d4f81aac70b4f7233364d34e6f41d053b
aptimizer <aptimizer.co@gmail.com>
2024-09-27 21:17:52 +0900

    [✨기능] useStartTest 커스텀훅 생성

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,5 +1,7 @@
+export { default as useCoupon } from "./useCoupon/useCoupon";
 export { default as useLogin } from "./useLogin/useLogin";
 export { default as useRegister } from "./useRegister/useRegister";
+export { default as useStartTest } from "./useStartTest/useStartTest";
 export { default as useQuizLogic } from "@/lib/hooks/useQuizLogic";
 export { default as useQuizzesLogic } from "@/lib/hooks/useQuizzesLogic";
 export { default as useTestLogic } from "@/lib/hooks/useTestLogic";
---
+++ src/lib/hooks/useStartTest/useStartTest.ts
@@ -0,0 +1,51 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/stores";
+
+/* 
+useStartTest
+
+fetch 통신 이후 useAuthStore에 유저 데이터와 액세스 토큰을 각각 저장해
+로그인 상태를 만들고 추가 회원정보 페이지 이동을 수행한다.
+*/
+
+const useStartTest = () => {
+  const { user, access_token } = useAuthStore();
+  const router = useRouter();
+
+  const handleTest = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/submit_responses_university`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${access_token}`,
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            user_id: user.user_id,
+          }),
+        }
+      );
+
+      const fetchResult = await response.json();
+
+      if (!response.ok) {
+        console.error("에러 발생:", fetchResult.error);
+        alert(fetchResult.error);
+        return;
+      }
+
+      console.log(fetchResult);
+    } catch (error) {
+      console.error("데이터 전송 중 오류가 발생했습니다:", error);
+    }
+  };
+
+  return { handleTest };
+};
+
+export default useStartTest;
