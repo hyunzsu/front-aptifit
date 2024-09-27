93c728788924150f3f2afb74a7eb638d91249f2b
aptimizer <aptimizer.co@gmail.com>
2024-09-27 20:23:05 +0900

    [✨기능] useAuthStore를 유저 데이터와 액세스 토큰 관리로 세분화

--- src/lib/stores/useAuthStore/useAuthStore.ts
+++ src/lib/stores/useAuthStore/useAuthStore.ts
@@ -1,33 +1,46 @@
 import { create } from "zustand";
 
-type TUser = {
-  name: string;
-  user_id: string;
-  IsPayment: boolean;
-  IsAdditionalUserInfo: boolean;
-  page: number;
-  responses?: number[];
-  questions?: string[];
-  result?: any;
-  access_token: string;
-};
+// type TUser = {
+//   name: string;
+//   user_id: string;
+//   IsPayment: boolean;
+//   IsAdditionalUserInfo: boolean;
+//   page: number;
+//   responses?: number[];
+//   questions?: string[];
+//   result?: any;
+//   access_token: string;
+// };
 
-type TAuthState = {
-  isLoggedIn: boolean;
-  login: (user: TUser) => void;
-  logout: () => void;
-};
+// type TAuthState = {
+//   isLoggedIn: boolean;
+//   login: (user: TUser) => void;
+//   logout: () => void;
+// };
 
 // Zustand 스토어 생성
-const useAuthStore = create<TAuthState>((set) => ({
-  isLoggedIn: !!sessionStorage.getItem("user"), // 세션스토리지를 바탕으로 초깃값을 두어 세션 스토리지에 값이 있다면 새로고침해도 false X
-  login: (userData) => {
+const useAuthStore = create((set) => ({
+  user: JSON.parse(sessionStorage.getItem("user")),
+  setUser: (userData) => {
     sessionStorage.setItem("user", JSON.stringify(userData));
-    set({ isLoggedIn: !!sessionStorage.getItem("user") });
+    set({
+      user: { ...userData },
+    });
   },
-  logout: () => {
+  removeUser: () => {
     sessionStorage.removeItem("user");
-    set({ isLoggedIn: !!sessionStorage.getItem("user") });
+    set({
+      user: JSON.parse(sessionStorage.getItem("user")),
+    });
+  },
+  access_token: JSON.parse(sessionStorage.getItem("access_token")),
+  setAccessToken: (tokenData) => {
+    sessionStorage.setItem("access_token", JSON.stringify(tokenData));
+    set({ access_token: tokenData });
+  },
+  removeAccessToken: () => {
+    sessionStorage.removeItem("access_token");
+    set({ access_token: JSON.parse(sessionStorage.getItem("access_token")) });
   },
 }));
 
