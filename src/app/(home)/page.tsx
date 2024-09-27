58dfd769a6d56e9d8b244ce8f539a0b7436e7d10
aptimizer <aptimizer.co@gmail.com>
2024-09-27 14:33:18 +0900

    [✨기능] 로그인 업데이트

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,48 +9,39 @@ import s from "./HomePage.module.css";
 
 export default function HomePage() {
   const router = useRouter();
-  const { isLoggedIn } = useAuthStore();
+  const user = JSON.parse(sessionStorage.getItem("user"));
 
   const startTest = () => {
-    const {
-      IsPayment,
-      IsAdditionalUserInfo,
-      page,
-      responses,
-      questions,
-      result,
-    } = isLoggedIn;
-
     // 1. 로그인 유무 파악 -> /login
-    if (!isLoggedIn) {
+    if (!user) {
       alert("로그인 페이지로 이동합니다!");
       router.push("/login");
       return;
     }
 
     // 2. 추가 정보 파악
-    if (!IsAdditionalUserInfo) {
+    if (!user.IsAdditionalUserInfo) {
       alert("아직 입력하지 않은 정보가 있습니다!");
       router.push("/add-user-info");
       return;
     }
 
     //3. 결제 유무 파악 -> /payment
-    if (!IsPayment) {
+    if (!user.IsPayment) {
       alert("결제정보가 없습니다!");
       router.push("/payment");
       return;
     }
 
     //4. 테스트 페이지 이동
-    if (page > 10 && responses && questions) {
+    if (user.page > 10 && user.responses && user.questions) {
       alert("테스트 페이지로 이동합니다!");
-      router.push(`/test/${page}`);
+      router.push(`/test/${user.page}`);
       return;
     }
 
     //5. 다 풀고, 결과지 데이터가 있으면 result로 이동
-    if (page === 10 && result) {
+    if (user.page === 10 && user.result) {
       alert("결과지 페이지로 이동합니다!");
       router.push(`/result`);
       return;
