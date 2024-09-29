b4dcee56ff89d639765667505ca66adfe9f00d1c
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-30 01:32:02 +0900

    [🎨UI] 테스트 퍼센티지 UI

---
+++ src/app/newtest/[id]/_components/Percentage/Percentage.tsx
@@ -0,0 +1,9 @@
+import s from "./Percentage.module.css";
+
+export default function Percentage() {
+  return (
+    <div className={s.Percentage}>
+      <span className={s.score}>100</span>%
+    </div>
+  );
+}
