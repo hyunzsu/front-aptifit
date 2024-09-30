8bb63b1dc808857682ca588ad319688c94f183d2
rlawlsdn263 <rlawlsdn263@naver.com>
2024-09-30 23:22:00 +0900

    [🎨UI] 뉴 테스트 페이지 동적 렌더링

--- src/app/newtest/[id]/_components/TestItem/TestItem.tsx
+++ src/app/newtest/[id]/_components/TestItem/TestItem.tsx
@@ -1,11 +1,9 @@
 import s from "./TestItem.module.css";
 
-export default function TestItem() {
+export default function TestItem({ question }) {
   return (
     <div className={s.TestItem}>
-      <p className={s.title}>
-        주말 저녁, 업무 관련 책을 읽기보다는 나를 위한 시간을 보내고 싶다
-      </p>
+      <p className={s.title}>{question}</p>
     </div>
   );
 }
--- src/app/newtest/[id]/page.tsx
+++ src/app/newtest/[id]/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 import { LayoutContainer } from "@/components";
 import {
   Title,
@@ -9,6 +13,30 @@ import {
 import s from "./NewTestPage.module.css";
 
 function NewTestPage() {
+  // sessionStorage.setItem(
+  //   "aptifit1",
+  //   JSON.stringify({
+  //     page: 1,
+  //     responses: [0, 0, 0],
+  //     questions: ["1번 질문", "2번 질문", "3번 질문"],
+  //   })
+  // );
+
+  const [responses, setResponses] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const savedData = JSON.parse(sessionStorage.getItem(`aptifit${id}`));
+
+    if (savedData) {
+      const { questions, responses } = savedData;
+
+      setResponses(responses);
+      setQuestions(questions);
+    }
+  }, [id]);
+
   return (
     <main className={s.NewTestPage}>
       <LayoutContainer>
@@ -22,18 +50,18 @@ function NewTestPage() {
         </header>
         {/* 02. 문제 */}
         <div className={s.TestBodySection}>
-          <div className={s.TestQuestion}>
-            <TestItem />
-            <TestOption />
-          </div>
-          <div className={s.TestQuestion}>
-            <TestItem />
-            <TestOption />
-          </div>
-          <div className={s.TestQuestion}>
-            <TestItem />
-            <TestOption />
-          </div>
+          {questions.map((question, index) => {
+            return (
+              <div className={s.TestQuestion} key={index}>
+                <TestItem question={question} />
+                <TestOption
+                  questionId={index}
+                  responses={responses}
+                  setResponses={setResponses}
+                />
+              </div>
+            );
+          })}
         </div>
       </LayoutContainer>
     </main>
