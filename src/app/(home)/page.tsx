bf4899343285fd9b99dfd2824a5e3eae6e5df0d7
aptimizer <aptimizer.co@gmail.com>
2024-09-30 12:37:22 +0900

    [🎨UI] 네비게이션 위치 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,6 +7,7 @@ import { MajorCardSlider } from "./_components";
 import s from "./HomePage.module.css";
 import { useAuthStore } from "@/lib/stores";
 import { useStartTest } from "@/lib/hooks";
+import { Navigation } from "@/components";
 
 export default function HomePage() {
   const { handleStartTest, handleContinueTest } = useStartTest();
@@ -45,37 +46,40 @@ export default function HomePage() {
   };
 
   return (
-    <main className={s.HomePage}>
-      <div className={s.titleContainer}>
-        <p className={s.subtitle}>AI 기반 진단도구로 쉽고 빠르게</p>
-        <p className={s.title}>너만의 전공적성을 찾아봐</p>
-        <div className={s.buttonContainer}>
-          <Button
-            label="시작하기"
-            type="button"
-            pageType="main"
-            onClick={startTest}
-          />
-          {user?.page && (
+    <>
+      <Navigation />
+      <main className={s.HomePage}>
+        <div className={s.titleContainer}>
+          <p className={s.subtitle}>AI 기반 진단도구로 쉽고 빠르게</p>
+          <p className={s.title}>너만의 전공적성을 찾아봐</p>
+          <div className={s.buttonContainer}>
             <Button
-              label="이어하기"
+              label="시작하기"
               type="button"
               pageType="main"
-              onClick={continueTest}
+              onClick={startTest}
             />
-          )}
+            {user?.page && (
+              <Button
+                label="이어하기"
+                type="button"
+                pageType="main"
+                onClick={continueTest}
+              />
+            )}
+          </div>
         </div>
-      </div>
-      <Image
-        className={s.iphone}
-        src="/imgs/iphone.png"
-        alt="아이폰"
-        width={320}
-        height={727}
-      />
-      <div className={s.cardAnimationContainer}>
-        <MajorCardSlider />
-      </div>
-    </main>
+        <Image
+          className={s.iphone}
+          src="/imgs/iphone.png"
+          alt="아이폰"
+          width={320}
+          height={727}
+        />
+        <div className={s.cardAnimationContainer}>
+          <MajorCardSlider />
+        </div>
+      </main>
+    </>
   );
 }
