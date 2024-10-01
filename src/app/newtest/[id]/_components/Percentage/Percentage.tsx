528043f3b675ae3a5633c4798d53959702099613
aptimizer <aptimizer.co@gmail.com>
2024-10-01 17:54:24 +0900

    [✨기능] 퍼센티지 업데이트 기능

--- src/app/newtest/[id]/_components/Percentage/Percentage.tsx
+++ src/app/newtest/[id]/_components/Percentage/Percentage.tsx
@@ -1,9 +1,17 @@
 import s from "./Percentage.module.css";
 
-export default function Percentage() {
+export default function Percentage({ responses }) {
+  // 0이 아닌 응답의 개수를 계산
+  const completedCount = responses.filter((response) => response !== 0).length;
+
+  // 전체 응답 중 완료된 비율 계산
+  const progressPercentage = Math.floor(
+    (completedCount / responses.length) * 100
+  );
+
   return (
     <div className={s.Percentage}>
-      <span className={s.score}>100</span>%
+      <span className={s.score}>{progressPercentage}</span>%
     </div>
   );
 }
