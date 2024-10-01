d309799571eae2231df74f7a36060e798515d963
aptimizer <aptimizer.co@gmail.com>
2024-10-01 16:07:21 +0900

    [🛠️리팩토링] /test/[id] 페이지 비즈니스 로직을 커스텀훅으로 전환

--- src/app/newtest/[id]/page.tsx
+++ src/app/newtest/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useParams } from "next/navigation";
+import { useTestNavigation, useTestQuestion, useTestScroll } from "@/lib/hooks";
 import { LayoutContainer } from "@/components";
 import {
   Title,
@@ -22,84 +21,14 @@ function NewTestPage() {
   //   })
   // );
 
-  const [responses, setResponses] = useState([]);
-  const [questions, setQuestions] = useState([]);
-  const { id } = useParams();
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [scrollTimeout, setScrollTimeout] = useState(null); // 연속 휠 방지
-
-  // 다음 질문으로 이동
-  const handleNextQuestion = () => {
-    if (currentIndex < questions.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-
-  // 이전 질문으로 이동
-  const handlePrevQuestion = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
-  // 스크롤로 질문 이동 및 스크롤 방지
-  const handleScroll = (e) => {
-    // 연속 스크롤 방지
-    if (scrollTimeout) return;
-
-    if (e.deltaY < 0) {
-      // 휠을 위로 스크롤 -> 이전 질문
-      handlePrevQuestion();
-    }
-
-    // if (e.deltaY > 0) {
-    //   // 휠을 아래로 스크롤 -> 다음 질문
-    //   handleNextQuestion();
-    // } else if (e.deltaY < 0) {
-    //   // 휠을 위로 스크롤 -> 이전 질문
-    //   handlePrevQuestion();
-    // }
-
-    // 짧은 시간 동안 연속 스크롤을 막기 위한 타이머 설정
-    const timeout = setTimeout(() => {
-      setScrollTimeout(null);
-    }, 800); // 0.8초 동안 연속 스크롤 방지
-    setScrollTimeout(timeout);
-  };
+  const { responses, setResponses, questions } = useTestQuestion();
+  const { currentIndex, handleNextQuestion, handlePrevQuestion } =
+    useTestNavigation(responses, questions);
+  const { handleScroll } = useTestScroll(handlePrevQuestion);
 
   const submitResponses = () => {
     console.log(responses);
   };
-
-  // responses, questions 업데이트
-  useEffect(() => {
-    const savedData = JSON.parse(sessionStorage.getItem(`aptifit${id}`));
-
-    if (savedData) {
-      const { questions, responses } = savedData;
-
-      setResponses(responses);
-      setQuestions(questions);
-    }
-  }, []);
-
-  // 안 푼 문제로 이동
-  useEffect(() => {
-    // responses 배열에서 값이 0인 (아직 풀지 않은 문제를 찾는다)
-    const unsolvedQuestion = responses.findIndex((r) => r === 0);
-
-    // 0이 없으면 (모든 문제가 풀렸으면) findIndex는 -1을 반환
-    if (unsolvedQuestion === -1) {
-      // currenIndex를 마지막 문제번호로 하기 위해 responses.length - 1로 업데이트
-      // 초깃값이 없어 -1이기 때문에 []에 responses를 넣는다.
-      // responses마다 업데이트가 일어나서 수정하기가 힘든데?
-      setCurrentIndex(responses.length - 1);
-      return;
-    }
-
-    setCurrentIndex(unsolvedQuestion);
-  }, [responses]);
 
   return (
     <main className={s.NewTestPage}>
