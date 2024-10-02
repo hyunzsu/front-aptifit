935ff822b7916c5908c337ecb3488e825dd2f217
aptimizer <aptimizer.co@gmail.com>
2024-10-02 10:49:16 +0900

    [🔫트러블슈팅] 세션스토리지 ReferenceError 해결
    세션스토리지 API는 브라우저 API로 서버 사이드 렌더링 환경에서는 존재하지 않아 ReferenceError가 발생한다. 그래서 if (typeof window) 조건을 추가해 현재 코드가 브라우저 환경에서 실행되는지 확인해줘야한다.

--- src/lib/utils/getDataFromSessionStorage.ts
+++ src/lib/utils/getDataFromSessionStorage.ts
@@ -1,7 +1,15 @@
-// 세션스토리지에서 데이터를 가져오는 함수
+/* 세션스토리지에 데이터 저장 */
 
 const getDataFromSessionStorage = (key: string) => {
-  return JSON.parse(sessionStorage.getItem(key));
+  if (typeof window !== "undefined") {
+    const savedData = sessionStorage.getItem(key);
+
+    if (savedData) {
+      return JSON.parse(savedData);
+    }
+  }
+
+  return null;
 };
 
 export default getDataFromSessionStorage;
