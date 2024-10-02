49076c2a4e1b9b63a7779b17c983bcaf78708545
aptimizer <aptimizer.co@gmail.com>
2024-10-02 11:07:28 +0900

    [🛡️방패] useAuthStore 파라미터 타입 추가

--- src/lib/stores/useAuthStore/useAuthStore.ts
+++ src/lib/stores/useAuthStore/useAuthStore.ts
@@ -4,32 +4,19 @@ import {
   saveDataToSessionStorage,
 } from "@/lib/utils";
 
-// type TUser = {
-//   name: string;
-//   user_id: string;
-//   IsPayment: boolean;
-//   IsAdditionalUserInfo: boolean;
-//   page: number;
-//   result?: any;
-//   access_token: string;
-// };
-
-// type TAuthState = {
-//   isLoggedIn: boolean;
-//   login: (user: TUser) => void;
-//   logout: () => void;
-// };
-
-// Zustand 스토어 생성
+/* useAuthStore - 사용자, 액세스 토큰 데이터를 전역으로 관리하는 상태변수 */
 const useAuthStore = create((set) => ({
+  // 1-1. user 스토어
   user: getDataFromSessionStorage("user"),
-  setUser: (userData) => {
+  // 1-2. user 스토어 업데이트
+  setUser: (userData: object) => {
     saveDataToSessionStorage("user", userData);
     set({
       user: { ...userData },
     });
   },
-  updateUser: (newData) => {
+  // 1-3. user 스토어 객체 합성
+  updateUser: (newData: object) => {
     const oldData = getDataFromSessionStorage("user");
     const updatedUser = { ...oldData, ...newData };
     saveDataToSessionStorage("user", updatedUser);
@@ -37,17 +24,21 @@ const useAuthStore = create((set) => ({
       user: updatedUser,
     });
   },
+  // 1-4. user 스토어 초기화
   removeUser: () => {
     sessionStorage.removeItem("user");
     set({
       user: getDataFromSessionStorage("user"),
     });
   },
+  // 2-1. access_token 스토어
   access_token: getDataFromSessionStorage("access_token"),
-  setAccessToken: (tokenData) => {
+  // 2-2. access_token 스토어 업데이트
+  setAccessToken: (tokenData: string) => {
     saveDataToSessionStorage("access_token", tokenData);
     set({ access_token: tokenData });
   },
+  // 2-3. access_token 스토어 초기화
   removeAccessToken: () => {
     sessionStorage.removeItem("access_token");
     set({ access_token: getDataFromSessionStorage("user") });
