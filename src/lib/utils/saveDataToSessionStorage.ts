deb87c0eb750ba1d3a2b11e78da6bd11eebeb9c1
aptimizer <aptimizer.co@gmail.com>
2024-10-02 10:52:17 +0900

    [🛡️타입] saveDataToSessionStorage 타입 수정
    value 파라미터의 타입을 any에서 보다 구체적인 string | number | object 유니온 타입으로 지정했다.

--- src/lib/utils/saveDataToSessionStorage.ts
+++ src/lib/utils/saveDataToSessionStorage.ts
@@ -1,6 +1,9 @@
 // 세션스토리지에서 데이터를 저장하는 함수
 
-const saveDataToSessionStorage = (key: string, value: any) => {
+const saveDataToSessionStorage = (
+  key: string,
+  value: string | number | object
+) => {
   sessionStorage.setItem(key, JSON.stringify(value));
 };
 
